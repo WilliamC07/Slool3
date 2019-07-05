f4c6ac8015b7a54d528967456485919c92dd3426
William Cao <williamcao999@gmail.com>
2019-07-05 15:11:42 -0400

    moved router boilerplate to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,5 +2,18 @@ import "./bootstrap";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/home';
+import {BrowserRouter, Router, Switch} from "react-router-dom";
+
+class App extends React.Component{
+    render(){
+        return (
+            <BrowserRouter>
+                <div>
+                    Hi
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
 
 ReactDOM.render(<Home/>, document.getElementById('head'));
--- resources/js/components/home.jsx
+++ resources/js/components/home.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Router, Switch} from "react-router-dom";
 
 export default class Home extends React.Component{
-    render(){
-        return (
-            <BrowserRouter>
-                <div>
-                    Hi
-                </div>
-            </BrowserRouter>
-        )
-    }
+    
 }
