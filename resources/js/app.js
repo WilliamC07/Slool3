b0eb97200734f4b76e06b5c145ff6f4810ba066c
William Cao <williamcao999@gmail.com>
2019-07-05 17:14:41 -0400

    fixed ReactDOM using wrong component for root

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,20 @@
-import "./bootstrap";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/home';
-import {BrowserRouter, Router, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Login from "./components/login/login";
+import CreateAccount from "./components/login/createAccount";
 
 class App extends React.Component{
     render(){
         return (
             <BrowserRouter>
-                <div>
-                    Hi
-                </div>
+                <Switch>
+                    {/*The user must be login in the view the website at all*/}
+                    <Route exact path = '/' component={CreateAccount}/>
+                </Switch>
             </BrowserRouter>
         )
     }
 }
 
-ReactDOM.render(<Home/>, document.getElementById('head'));
+ReactDOM.render(<App/>, document.getElementById('head'));
